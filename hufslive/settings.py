0a5d7035bb9c06617a7e147cc970ff694d4a91cc
unknown <sjjoseph106@gmail.com>
2021-08-06 13:33:03 +0900

    PostgreSQL DB 연동

--- hufslive/settings.py
+++ hufslive/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'hufslive.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hufslive', # DB이름
+        'USER': 'postgres',
+        'PASSWORD': 'boombaboo',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
