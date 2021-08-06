92be8cfa524f696a648f7737b0cb746aebc1fd29
unknown <sjjoseph106@gmail.com>
2021-08-06 18:59:40 +0900

    게시판별 댓글 초기화

--- hufslive/settings.py
+++ hufslive/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oquhews37_r7*0+t!+7zi2rpx!pfmzp$coh@$*j=638rd@m(_@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
