88e494857a95c11d7f2a4298c23d7ebde5bcc5ef
unknown <sjjoseph106@gmail.com>
2021-08-10 14:38:45 +0900

    .

--- hufslive/settings.py
+++ hufslive/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'hufslive.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hufslive', # DB이름
+        'USER': 'postgres',
+        'PASSWORD': 'boombaboo',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
